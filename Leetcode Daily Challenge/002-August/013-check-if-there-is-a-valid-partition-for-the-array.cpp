be3b87b6c83bf7e0afc3fd7d261b3acd63b35d7a
Abhishek Kumar <abhixsliet@gmail.com>
2023-08-13 23:24:49 +0530

    13th August Daily Challenge Solved

---
+++ Leetcode Daily Challenge/002-August/013-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,70 @@
+// https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    bool solveRec(vector<int>& nums, int i) {
+        int n = nums.size();
+        // base case
+        if (i >= n) return 1;
+        
+        bool result = 0;
+        // Rule - 1 
+        if (i+1 < n && nums[i] == nums[i+1])
+            result = solveRec(nums, i+2);
+        
+        if (result) 
+            return result;
+        
+        // Rule - 2
+        if (i+2 < n && nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) 
+            result = solveRec(nums, i+3);
+        
+        if (result)
+            return result;
+
+        // Rule - 3
+        if (i+2 < n && nums[i+1] - nums[i] == 1 && nums[i+2] - nums[i+1] == 1) 
+            result = solveRec(nums, i+3);
+        
+        return result; 
+    }
+
+    bool solveMem(vector<int>& nums, int i, vector<int>& dp) {
+        int n = nums.size();
+        // base case
+        if (i >= n) return 1;
+
+        if (dp[i] != -1) return dp[i];
+        
+        bool result = 0;
+        // Rule - 1 
+        if (i+1 < n && nums[i] == nums[i+1])
+            result |= solveMem(nums, i+2, dp);
+
+        // Rule - 2
+        if (i+2 < n && nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) 
+            result |= solveMem(nums, i+3, dp);
+
+        // Rule - 3
+        if (i+2 < n && nums[i+1] - nums[i] == 1 && nums[i+2] - nums[i+1] == 1) 
+            result |= solveMem(nums, i+3, dp);
+        
+        return dp[i] = result; 
+    }
+
+public:
+    bool validPartition(vector<int>& nums) {
+        // // Recursive Solution -> TLE
+        // return solveRec(nums, 0);
+
+
+        // Recursion + Memoization
+        vector<int> dp(nums.size()+1, -1);
+        return solveMem(nums, 0, dp);
+    }
+};
