391568661e41827cbf04a2ca13fcbfc188b4f8be
Abhishek Kumar <abhixsliet@gmail.com>
2023-08-14 23:37:12 +0530

    14th August Daily Challenge Solved

---
+++ Leetcode Daily Challenge/002-August/014-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int num : nums) {
+            pq.push(num);
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+
+        return pq.top();
+    }
+};
