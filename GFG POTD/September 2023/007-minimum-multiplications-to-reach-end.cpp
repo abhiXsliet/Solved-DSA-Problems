9f7014ea451b6ec2405dd3a0450c5f321b447143
Abhishek Kumar <abhixsliet@gmail.com>
2023-09-07 18:51:38 +0530

    07th September POTD Solved

---
+++ GFG POTD/September 2023/007-minimum-multiplications-to-reach-end.cpp
@@ -0,0 +1,65 @@
+// https://practice.geeksforgeeks.org/problems/minimum-multiplications-to-reach-end/1
+
+
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        queue<pair<int, int>> q;
+        q.push({start, 0});
+        
+        vector<int> dist(100000, 1e9);
+        dist[start] = 0;
+        int mod = 100000;
+        
+        while(!q.empty()) {
+            int node  = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            
+            for (auto it : arr) {
+                int num = (it * node) % mod;
+                if (steps + 1 < dist[num]) {
+                    dist[num] = steps + 1;
+                    q.push({num, steps+1});
+                }
+            }
+        }
+        
+        if (dist[end] == 1e9) return -1;
+        else
+            return dist[end];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        int start, end;
+        cin >> start >> end;
+        Solution obj;
+        cout << obj.minimumMultiplications(arr, start, end) << endl;
+    }
+}
+
+// } Driver Code Ends
