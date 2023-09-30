9b92dc077e0b4079dd9214893c5d6b74af8e9285
Abhishek Kumar <abhixsliet@gmail.com>
2023-09-30 23:40:36 +0530

    030th September POTD Solved

---
+++ GFG POTD/September 2023/030-boolean-matrix.cpp
@@ -0,0 +1,87 @@
+// https://practice.geeksforgeeks.org/problems/boolean-matrix-problem-1587115620/1
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution
+{   
+private:
+    // TC = O(Row * Col)
+    // SC = O(Row + Col)
+    void solve_better(vector<vector<int> > &matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 1) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 1;
+                }
+            }
+        }
+    }
+
+public:
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(vector<vector<int> > &matrix)
+    {
+        solve_better(matrix);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        ob.booleanMatrix(matrix);
+
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
