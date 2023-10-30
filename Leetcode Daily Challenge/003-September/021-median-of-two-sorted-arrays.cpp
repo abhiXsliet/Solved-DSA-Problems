b4babbe3b90798edd86700eb4f0fc3d8a5eb45f1
Abhishek Kumar <abhixsliet@gmail.com>
2023-10-30 11:03:18 +0530

    021st September Daily Challenge Modified

--- Leetcode Daily Challenge/003-September/021-median-of-two-sorted-arrays.cpp
+++ Leetcode Daily Challenge/003-September/021-median-of-two-sorted-arrays.cpp
@@ -90,10 +90,56 @@ private:
         }
     }
 
+    // TC = O(min(logm, logn))
+    // SC = O(1)
+    double solve_optimal(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+
+        // Always choose the smallest array for making left half
+        if (m > n) return solve_optimal(nums2, nums1);
+
+        int low  = 0;
+        int high = m;
+
+        // no. of elements in the left halfe
+        int left = (m + n + 1) / 2;
+
+        int size = m + n;
+
+        while(low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+
+            if (mid1 < m) r1 = nums1[mid1];
+            if (mid2 < n) r2 = nums2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+
+                if (size % 2 == 0) {
+                    return (double)((max(l1, l2) + min(r1, r2)) / 2.0);
+                }
+                else {
+                    return max(l1, l2);
+                }
+            }
+            else if (l1 > r2) high = mid1 - 1;
+            else low =  mid1 + 1;
+        }
+        return 0;
+    }
+
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         // return solve_brute(nums1, nums2);
 
-        return solve_better(nums1, nums2);
+        // return solve_better(nums1, nums2);
+
+        return solve_optimal(nums1, nums2);
     }
 };
