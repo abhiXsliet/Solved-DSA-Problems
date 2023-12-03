6d0b35f4515dab6e2d0c09259eff1462c526736d
Abhishek Kumar <abhixsliet@gmail.com>
2023-12-03 17:21:25 +0530

    003rd December Daily Challenge Solved

---
+++ Leetcode Daily Challenge/006-December/003-minimum-time-visiting-all-points.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/minimum-time-visiting-all-points/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size();
+
+        int steps = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+
+            int x2 = points[i + 1][0];
+            int y2 = points[i + 1][1];
+
+            int dx = abs(x2 - x1);
+            int dy = abs(y2 - y1);
+
+            steps += (min(dx, dy) + abs(dy - dx));
+        }
+
+        return steps;
+    }
+};
