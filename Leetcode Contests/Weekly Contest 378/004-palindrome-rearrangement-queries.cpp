f9afbff71870007447d5310db77a4a4c8009dcac
Abhishek Kumar <abhixsliet@gmail.com>
2023-12-31 23:58:00 +0530

    Leetcode Contest Section Added - 031st December 2023

---
+++ Leetcode Contests/Weekly Contest 378/004-palindrome-rearrangement-queries.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/weekly-contest-378/problems/palindrome-rearrangement-queries/
+
+
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+class Solution {
+public:
+    vector<bool> canMakePalindromeQueries(string s, vector<vector<int>>& queries) {
+        
+    }
+};
