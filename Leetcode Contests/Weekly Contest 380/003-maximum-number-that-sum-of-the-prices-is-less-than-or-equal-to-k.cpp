5c353ec6590cdc650e89e96f6f6f2813620bfb94
Abhishek Kumar <abhixsliet@gmail.com>
2024-01-14 11:56:13 +0530

    Leetcode Contest Section Added - 014th January 2024 - Question - 3

---
+++ Leetcode Contests/Weekly Contest 380/003-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/weekly-contest-380/problems/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/
+
+
+
+#include <bits/stdc++.h>   
+using namespace std;
+
+class Solution {
+public:
+    long long findMaximumNumber(long long k, int x) {
+        
+    }
+};
