e1d25e982099be307449f1d228913799557c08c9
Abhishek Kumar <abhixsliet@gmail.com>
2024-01-24 03:21:15 +0530

    Leetcode Contest Section Added - 021st January 2024 - Question - 4

---
+++ Leetcode Contests/Weekly Contest 381/004-count-the-number-of-houses-at-a-certain-distance-ii.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/weekly-contest-381/problems/count-the-number-of-houses-at-a-certain-distance-ii/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<long long> countOfPairs(int n, int x, int y) {
+        
+    }
+};
