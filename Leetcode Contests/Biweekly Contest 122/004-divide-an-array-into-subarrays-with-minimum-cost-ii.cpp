9a3ce8f78682b93a748d04f0e2b75ab7697b7bbe
Abhishek Kumar <abhixsliet@gmail.com>
2024-01-27 03:07:56 +0530

    Leetcode Contest Section Added - 020th January 2024 - Question - 4

---
+++ Leetcode Contests/Biweekly Contest 122/004-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/biweekly-contest-122/problems/divide-an-array-into-subarrays-with-minimum-cost-ii/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+        
+    }
+};
