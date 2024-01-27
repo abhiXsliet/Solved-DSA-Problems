9bacd9a0566e7f9c26e2ea49eb7e26f46698c2dc
Abhishek Kumar <abhixsliet@gmail.com>
2024-01-28 01:39:15 +0530

    027th January 2024 Daily Challenge Modified

--- Leetcode Daily Challenge/007-January-2024/027-k-inverse-pairs-array.cpp
+++ Leetcode Daily Challenge/007-January-2024/027-k-inverse-pairs-array.cpp
@@ -64,6 +64,35 @@ private:
 
         return t[n][k];
     }
+
+    // TC : O(N * N)
+    // SC : O(N * K)
+    int solve_tab_optimized(int n, int k) {
+        // t[i][j] = total n. of arrays having (1 to i) & exactly j inversion
+        vector<vector<int>> t(n+1, vector<int>(k+1, 0));
+
+        // for j = 0, t[i][0] = 1
+        for (int i = 0; i <= n; i++) {
+            t[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) { // n is replaced with i
+
+            long long cummSum = 1;
+            for (int j = 1; j <= k; j++) {  // k is replaced with j
+
+                cummSum += t[i - 1][j];
+
+                if (j >= i) {
+                    cummSum -= t[i - 1][j - i];
+                }
+
+                t[i][j] = cummSum % MOD;    
+            }
+        }
+
+        return t[n][k];
+    }
 public:
     int kInversePairs(int n, int k) {
         // return solve_rec(n, k);
@@ -71,6 +100,8 @@ public:
         // memset(t, -1, sizeof(t));
         // return solve_mem(n, k);
 
-        return solve_tab(n, k);
+        // return solve_tab(n, k);
+
+        return solve_tab_optimized(n, k);
     }
 };
