62b2ec50de0abbd529db5bc3e6d8dc6f35711b04
Abhishek Kumar <abhixsliet@gmail.com>
2024-01-28 18:25:19 +0530

    Leetcode Contest Section Added - 028th January 2024 - Question - 4

---
+++ Leetcode Contests/Weekly Coontest 382/004-minimize-or-of-remaining-elements-using-operations.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/weekly-contest-382/problems/minimize-or-of-remaining-elements-using-operations/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOrAfterOperations(vector<int>& nums, int k) {
+        
+    }
+};
