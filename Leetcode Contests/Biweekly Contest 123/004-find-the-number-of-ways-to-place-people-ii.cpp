748226ad819ef91aff4a3240483e479dcd62b6c3
Abhishek Kumar <abhixsliet@gmail.com>
2024-02-05 16:10:54 +0530

    Leetcode Contest Section Added - 03rd February 2024 - Question - 4

---
+++ Leetcode Contests/Biweekly Contest 123/004-find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/biweekly-contest-123/problems/find-the-number-of-ways-to-place-people-ii/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        
+    }
+};
