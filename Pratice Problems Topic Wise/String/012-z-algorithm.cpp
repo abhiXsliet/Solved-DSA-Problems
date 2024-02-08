8f8e25094e5bd81b0cf0d5338c161232cc40e9ac
Abhishek Kumar <abhixsliet@gmail.com>
2024-02-09 03:47:10 +0530

    String Section Modified

--- Pratice Problems Topic Wise/String/012-z-algorithm.cpp
+++ Pratice Problems Topic Wise/String/012-z-algorithm.cpp
@@ -22,21 +22,61 @@ class Solution
             return ans;
         }
         
-        // TC : O(N + M) Where 'N' is the size of the text and 'M' is the size of the pattern
+        // TC : O(Quadratic) M^2 Where M is the length of concatenated new_string 
         // SC : O(N + M) Where 'N' is the size of text
+        vector<int> z_brute(string& pat, string& txt) {
+            vector<int> z;
+            string new_str = pat + "#" + txt;
+            int len    = new_str.length();
+            int i      = 0;
+            
+            while (i < len) {
+                if (i <= pat.length()) z.push_back(0);
+                else {
+                    int left  = 0, right = 0;
+                    if (new_str[left] == new_str[i]) {
+                        right = i;
+                        
+                        while (new_str[left] == new_str[right] && right < len) {
+                            left ++;
+                            right++;
+                        }
+                    }
+                    z.push_back(left);
+                }
+                i++;
+            }
+            
+            vector<int> result;
+            for (int i = 0; i < len; i++) {
+                if (z[i] == pat.length()) {
+                    result.push_back(i - pat.length());
+                }   
+            }
+            return result;
+        }
+        
+        // TC : O(N + M)
+        // SC : O(N + M)
+        // OPTIMAL APPROACH
         vector<int> z_function(string& pat, string& txt) {
             string new_str = pat + "#" + txt;
             int len = new_str.length();
             
             vector<int> z(len, 0);
             
-            for (int i = 1, l = 0, r = 0; i < len; ++i) {
-                if (i <= r)
+            for (int i = 1, l = 0, r = 0; i < len; i++) {
+                if (i <= r) {
                     z[i] = min(r - i + 1, z[i - l]);
+                }
+                
                 while (i + z[i] < len && new_str[z[i]] == new_str[i + z[i]])
-                    ++z[i];
-                if (i + z[i] - 1 > r)
-                    l = i, r = i + z[i] - 1;
+                    z[i]++;
+                    
+                if (i + z[i] - 1 > r) {
+                    l = i;
+                    r = i + z[i] - 1; 
+                }
             }
             
             vector<int> result;
@@ -52,6 +92,8 @@ class Solution
         vector <int> search(string pat, string txt)
         {
             // return solve_brute(pat, txt);
+            
+            // return z_brute(pat, txt);
             
             return z_function(pat, txt);
         }
@@ -78,6 +120,5 @@ int main()
     }
     return 0;
 }
-
 
 // } Driver Code Ends
