b7e05183352842375ea1b51cec2ad5ed90178156
Abhishek Kumar <abhixsliet@gmail.com>
2024-02-09 10:21:01 +0530

    09th February 2024 POTD Solved

---
+++ GFG POTD/002-February-2024/009-children-sum-parent.cpp
@@ -0,0 +1,213 @@
+// https://www.geeksforgeeks.org/problems/children-sum-parent/1
+
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+    private:
+    // TC : O(N)
+    // SC : O(H)
+    int solve_approach_1(Node* root) {
+        if(!root) return true; 
+         
+        if (root -> left != NULL && root -> right != NULL) {
+            root -> data = (root -> data - (root -> left -> data)) - (root -> right -> data);
+            
+            if (root -> data != 0) return false;
+        }
+        else if (root -> left != NULL && root -> right == NULL) {
+            root -> data = (root -> data - (root -> left -> data));
+            
+            if (root -> data != 0) return false;
+        }
+        else if (root -> left == NULL && root -> right != NULL) {
+            root -> data = (root -> data - (root -> right -> data));
+            
+            if (root -> data != 0) return false;
+        } else return true;
+        
+        int left  = solve_approach_1(root -> left);
+        int right = solve_approach_1(root -> right);
+        
+        return (left && right);
+    }
+    
+    // TC : O(N)
+    // SC : O(H)
+    int solve_approach_2(Node* root) {
+        if (!root || !root -> left && !root -> right)
+            return true;
+            
+        int left  = root -> left  ? root -> left  -> data : 0;
+        int right = root -> right ? root -> right -> data : 0;
+        
+        return (root -> data == left + right) && 
+               (solve_approach_2(root -> left)) && 
+               (solve_approach_2(root -> right));
+    }
+    
+    // TC : O(N)
+    // SC : O(H) -> Queue ends up storing depth of tree nodes
+    int solve_approach_3(Node* root) {
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()) {
+            Node* fNode = q.front();
+            q.pop();
+            
+            int sum = 0;
+            if (fNode -> left) {
+                q.push(fNode -> left);
+                
+                sum += fNode -> left -> data;
+            }
+            
+            if (fNode -> right) {
+                q.push(fNode -> right);
+                sum += fNode -> right -> data;
+            }
+            
+            if (!fNode -> left && !fNode -> right) 
+                continue;
+            
+            if (fNode -> data != sum) 
+                return false;
+        }
+        
+        return true;
+    }
+    public:
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes.
+    int isSumProperty(Node *root) {
+        // return solve_approach_1(root);
+        
+        // return solve_approach_2(root);
+        
+        return solve_approach_3(root);
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.isSumProperty(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
