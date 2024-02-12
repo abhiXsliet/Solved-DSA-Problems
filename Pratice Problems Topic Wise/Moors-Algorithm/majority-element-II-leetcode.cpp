ad75e33ca82431b02744760d5f81a2d27d1a34f8
Abhishek Kumar <abhixsliet@gmail.com>
2024-02-12 16:19:21 +0530

    Modified Problem Moore-Algorithm

--- Pratice Problems Topic Wise/Moors-Algorithm/majority-element-II-leetcode.cpp
+++ Pratice Problems Topic Wise/Moors-Algorithm/majority-element-II-leetcode.cpp
@@ -1,18 +1,21 @@
 // https://leetcode.com/problems/majority-element-ii/
 
 
+
 #include<bits/stdc++.h>
 using namespace std;
+
 class Solution {
-public:
-    vector<int> majorityElement(vector<int>& nums) {
-/* 
-        //TC => O(N*log(N))
-        //SC => O(N)
+private:
+    // TC : O(N)
+    // SC : O(N)
+    vector<int> solve_brute(vector<int>& nums, int n) {
         vector<int> ans;
-        int n = nums.size();
+
         int m = (int)(n/3) + 1;
-        map<int, int>mpp;
+
+        unordered_map<int, int>mpp;
+
         for(int i = 0; i < n; i++) {
             mpp[nums[i]]++;
 
@@ -20,17 +23,19 @@ public:
             if(ans.size() == 2)break;
         }
         return ans;
+    }
 
-         */
-        
-
-        //TC => O(N)
-        //SC => O(1)
+    // TC : O(N)
+    // SC : O(1)
+    vector<int> solve_optimal_1(vector<int>& nums, int n) {
         vector<int> ans;
-        int n = nums.size();
+
         int m = (int)(n/3) + 1;
+
         int cnt1 = 0, cnt2 = 0;
+
         int ele1 = INT_MIN, ele2 = INT_MIN;
+        
         for(int i = 0; i < n; i++) {
             if(cnt1 == 0 && ele2 != nums[i]) {
                 cnt1 = 1;
@@ -57,5 +62,52 @@ public:
         if(cnt2 >= m) ans.push_back(ele2);
 
         return ans;
+    }
+
+    // TC : O(N)
+    // SC : O(1)
+    vector<int> solve_optimal_2(vector<int>& nums, int n) {
+        int cnt1 = 0, cnt2 = 0;
+
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+        
+        for(int i = 0; i < n; i++) {
+            if      (nums[i] == ele1) cnt1 ++;
+            else if (nums[i] == ele2) cnt2 ++;
+            else if (cnt1 == 0) {
+                cnt1 = 1;
+                ele1 = nums[i];
+            }
+            else if (cnt2 == 0) {
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else {
+                cnt1 --;
+                cnt2 --;
+            }
+        }
+        
+        // Atmost 2-Elements can occur more than ⌊ n/3 ⌋ in an array of length n
+        vector<int> ans; 
+
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0; i < n; i++) {
+            if(ele1 == nums[i]) cnt1 ++;
+            if(ele2 == nums[i]) cnt2 ++;
+        }
+
+        if(cnt1 > floor(n/3)) ans.push_back(ele1);
+        if(cnt2 > floor(n/3)) ans.push_back(ele2);
+
+        return ans;
+    }
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        // return solve_brute(nums, nums.size());
+
+        // return solve_optimal_1(nums, nums.size());
+
+        return solve_optimal_2(nums, nums.size());
     }
 };
