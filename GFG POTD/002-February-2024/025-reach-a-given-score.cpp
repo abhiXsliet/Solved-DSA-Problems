ec5a96b466834d162e464589ccd1fc15c5089c8d
Abhishek Kumar <abhixsliet@gmail.com>
2024-02-25 23:39:39 +0530

    25th February 2024 POTD Solved

---
+++ GFG POTD/002-February-2024/025-reach-a-given-score.cpp
@@ -0,0 +1,55 @@
+// https://www.geeksforgeeks.org/problems/reach-a-given-score-1587115621/1
+
+
+
+//{ Driver Code Starts
+// Driver Code
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    // TC : O(N)
+    // SC : O(N)
+    // Complete this function
+    long long int count(long long int n)
+    {
+        vector<int> dp(n+1, 0);
+        
+        dp[0] = 1;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] += dp[i - 3];
+        }
+        
+        for (int i = 5; i <= n; i++) {
+            dp[i] += dp[i - 5];
+        }
+        
+        for (int i = 10; i <= n; i++) {
+            dp[i] += dp[i - 10];
+        }
+        
+        return dp[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		Solution obj;
+		cout<<obj.count(n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
