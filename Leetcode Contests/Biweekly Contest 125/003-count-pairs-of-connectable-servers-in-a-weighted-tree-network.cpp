2b560ac636e26a2709c719b105a075149a59e816
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-03 10:21:27 +0530

    Leetcode Contest Section Added - 02nd March 2024 - Question - 3

---
+++ Leetcode Contests/Biweekly Contest 125/003-count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/biweekly-contest-125/problems/count-pairs-of-connectable-servers-in-a-weighted-tree-network/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges, int signalSpeed) {
+        
+    }
+};
