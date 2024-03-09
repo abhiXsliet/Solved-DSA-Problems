7b5684f860dc783d5283716db08af07a75c9aed6
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-09 18:37:56 +0530

    Leetcode Contest Section Added - 03rd March 2024 - Question - 4

---
+++ Leetcode Contests/Weekly Contest 387/004-distribute-elements-into-two-arrays-ii.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/weekly-contest-387/problems/distribute-elements-into-two-arrays-ii/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        
+    }
+};
