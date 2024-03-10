a52195f6914f1a0b0ff77715d15f33ed267c1edb
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-10 10:54:29 +0530

    Leetcode Contest Section Modified - 10th March 2024 - Question - 1

--- Leetcode Contests/Weekly Contest 388/001-apple-redistribution-into-boxes.cpp
+++ Leetcode Contests/Weekly Contest 388/001-apple-redistribution-into-boxes.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 class Solution {
-public:
-    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+private:
+    int solve_approach_1(vector<int>& apple, vector<int>& capacity) {
         sort(capacity.rbegin(), capacity.rend());
         int sum = accumulate(begin(apple), end(apple), 0);
         int cnt = 0;
@@ -19,5 +19,29 @@ public:
             if (sum <= 0) break;
         }
         return cnt;
+    }
+    
+    int solve_approach_2(vector<int>& apple, vector<int>& capacity) {
+        priority_queue<int> pq;
+        for (int cap : capacity) {
+            pq.push(cap);
+        }
+        int sum = accumulate(begin(apple), end(apple), 0);
+        int cnt = 0;
+        while (!pq.empty()) {
+            if (sum > 0) {
+                cnt ++;
+                int el = pq.top();
+                sum -= el; pq.pop();
+            }
+            if (sum <= 0) break;
+        }
+        return cnt;
+    }
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        // return solve_approach_1(apple, capacity);
+        
+        return solve_approach_2(apple, capacity);
     }
 };
