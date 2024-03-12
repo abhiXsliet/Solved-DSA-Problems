d50b3cd620fc1ab2aff870c40e906576103a8c00
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-12 11:02:47 +0530

    Leetcode Contest Section Modified - 10th March 2024 - Question - 3

--- Leetcode Contests/Weekly Contest 388/003-shortest-uncommon-substring-in-an-array.cpp
+++ Leetcode Contests/Weekly Contest 388/003-shortest-uncommon-substring-in-an-array.cpp
@@ -6,11 +6,10 @@
 using namespace std;
 
 class Solution {
-public:
+private:
     // TC : O(n * (m^3 + m^2*log(m) + m^2*n*m)) ~= O(n^2 * m^2)
-    // SC : O(N) + O(m^2) Where N = arr.size() & M = max length of individual strings of arr
-    vector<string> shortestSubstrings(vector<string>& arr) {
-        int n = arr.size();
+    // SC : O(n * m^2) Where N = arr.size() & M = max length of individual strings of arr
+    vector<string> approach_1(vector<string>& arr, int n) {
         vector<string> res(n, "");
         for (int i = 0; i < n; i++) {
             vector<string> st;
@@ -46,5 +45,82 @@ public:
             }
         }
         return res;
+    }
+
+    // TC : O(n^2 * m^2)
+    // SC : O(n*m^2)
+    vector<string> approach_2(vector<string>& arr, int n) {
+        vector<string> answer(n, string(21, 'z'));
+        for (int i = 0; i < n; ++i) {
+            unordered_set<string> substrings;
+            for (int len = 1; len <= arr[i].size(); ++len) {
+                for (int start = 0; start + len <= arr[i].size(); ++start) {
+                    substrings.insert(arr[i].substr(start, len));
+                }
+            }
+            for (const string& s : substrings) {
+                bool unique = true;
+                for (int j = 0; j < n; ++j) {
+                    if (i != j && arr[j].find(s) != string::npos) {
+                        unique = false;
+                        break;
+                    }
+                }
+                if (unique && (s.size() < answer[i].size() || (s.size() == answer[i].size() && s < answer[i]))) {
+                    answer[i] = s;
+                }
+            }
+            if (answer[i] == string(21, 'z')) { 
+                answer[i] = "";
+            }
+        }
+        return answer;
+    }
+
+    // TC : ~O(n^4)
+    // SC : O(n^3) storing all unique substrings in seen (In worst case we will have n^2 substrings from each string (suppose of length n) and let's we have n string in total
+    vector<string> approach_3(vector<string>& arr, int n) {
+        vector<string> answer(n);
+        unordered_map<string, int> mpp;
+        
+        for (string& s : arr) {
+            
+            unordered_set<string> seen;     // individual string substrings should be present only once
+            for (int i = 0; i < s.length(); i++) {
+                for (int len = 1; len <= s.length(); len++) {
+                    string substr = s.substr(i, len);
+                    if (seen.find(substr) == seen.end()) {
+                        seen.insert(substr);
+                        mpp[substr] ++;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            string shortest = "";
+            
+            for (int j = 0; j < arr[i].length(); j++) {
+                for (int len = 1; len <= arr[i].length(); len++) {
+                    string substr = arr[i].substr(j, len);
+                    
+                    if (mpp[substr] == 1 && (shortest == "" || shortest.length() > substr.length() || 
+                        (shortest.length() == substr.length() && shortest > substr) ) ) {
+                        shortest = substr;
+                    }
+                }
+            }
+
+            answer[i] = shortest;
+        }
+        return answer;
+    }
+public:
+    vector<string> shortestSubstrings(vector<string>& arr) {
+        // return approach_1(arr, arr.size());
+
+        // return approach_2(arr, arr.size());
+        
+        return approach_3(arr, arr.size());
     }
 };
