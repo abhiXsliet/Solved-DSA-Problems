32d4af8dbeb58d360bd06cbbce66223a136ceee5
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-17 01:05:32 +0530

    Leetcode Contest Section Added - 17th March 2024 - Question - 4

---
+++ Leetcode Contests/Biweekly Contest 126/004-find-the-sum-of-the-power-of-all-subsequences.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/biweekly-contest-126/problems/find-the-sum-of-the-power-of-all-subsequences/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int sumOfPower(vector<int>& nums, int k) {
+        
+    }
+};
