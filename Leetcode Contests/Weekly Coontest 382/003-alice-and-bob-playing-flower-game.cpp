d755d2c90f0b4c5eda174d9a90ee03b4cbd94690
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-16 16:39:16 +0530

    Leetcode Contest Section Modified - 028th January 2024 - Question - 3

--- Leetcode Contests/Weekly Coontest 382/003-alice-and-bob-playing-flower-game.cpp
+++ Leetcode Contests/Weekly Coontest 382/003-alice-and-bob-playing-flower-game.cpp
@@ -6,8 +6,24 @@
 using namespace std;
 
 class Solution {
+private:
+    typedef long long ll;
+
+    // TC : O(1)
+    // SC : O(1)
+    ll solve_1(int n, int m) {
+        ll odd_n  = n / 2;          // odd numbers cnt from 1 to n  
+        ll even_n = (n + 1) / 2;    // even numbers cnt from 1 to n
+
+        ll odd_m  = m / 2;          // odd numbers cnt from 1 to n
+        ll even_m = (m + 1) / 2;    // even numbers cnt from 1 to m
+
+        return (odd_n * even_m) + (even_n * odd_m);
+    }
 public:
     long long flowerGame(int n, int m) {
-        
+        // return solve_1(n, m);
+
+        return (static_cast<ll>(n) * (m)) / 2;
     }
 };
