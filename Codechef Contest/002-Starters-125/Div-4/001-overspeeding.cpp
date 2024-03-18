a65f40de0165b135c86b0d2bc90554386e30a773
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-18 23:27:10 +0530

    13th March 2024 - Div-4 - Question - 1 Added

---
+++ Codechef Contest/002-Starters-125/Div-4/001-overspeeding.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/CCOV
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int s;
+	cin>>s;
+	if(s > 40) cout <<"YES" << endl; 
+	else cout <<"NO" << endl;
+}
