2f7d10cd5bddcab8bf7e1711f96e664ceeb90acf
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-18 23:22:39 +0530

    06th March 2024 - Div-4 - Question - 1 Added

---
+++ Codechef Contest/001-Starters-124/Div-4/001-summer-time.cpp
@@ -0,0 +1,12 @@
+// https://www.codechef.com/problems/MANGOLASSI
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int temp;
+	cin >> temp;
+    if (temp > 35) cout << "YES";
+    else cout << "NO";
+}
