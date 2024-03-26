79d698a4466a8e4cf7d9d4bad8526cb6b31ed4e5
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-27 01:41:38 +0530

    20th March 2024 - Div-4 - Question - 1 Added

---
+++ Codechef Contest/003-Starters-126/Div-4/001-ac-please.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/ACPLZ
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	if(t > 30) cout << "YES" << endl;
+	else cout << "NO" << endl; 
+}
