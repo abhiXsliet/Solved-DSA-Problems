3cd2e68dfad5653282c2c67d365f17a65d5e41c9
Abhishek Kumar <abhixsliet@gmail.com>
2024-03-29 23:39:54 +0530

    Leetcode Contest Section Added - 017th March 2024 - Question - 4

---
+++ Leetcode Contests/Weekly Contest 389/004-minimum-moves-to-pick-k-ones.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/contest/weekly-contest-389/problems/minimum-moves-to-pick-k-ones/
+
+
+
+#include <bits/stdc++.h>    
+using namespace std;
+
+class Solution {
+public:
+    long long minimumMoves(vector<int>& nums, int k, int maxChanges) {
+        
+    }
+};
