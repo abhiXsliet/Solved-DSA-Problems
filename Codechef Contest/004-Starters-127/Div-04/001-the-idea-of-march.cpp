9de9fa4c89117774d32fe09744cc15eda1dbc9c1
Abhishek Kumar <abhixsliet@gmail.com>
2024-04-01 22:43:11 +0530

    27th March 2024 - Div-4 - Question - 1 Added

---
+++ Codechef Contest/004-Starters-127/Div-04/001-the-idea-of-march.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/IDESM
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int d;
+	cin>>d;
+	if(d == 15) cout << "Yes" << endl;
+	else cout << "No" << endl;
+}
