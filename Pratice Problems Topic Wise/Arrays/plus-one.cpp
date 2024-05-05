fa342577aa45a8fefd71dc5c2f4d88f0f4f80925
Abhishek Kumar <abhixsliet@gmail.com>
2024-05-05 11:52:29 +0530

    Solved Array Problem

---
+++ Pratice Problems Topic Wise/Arrays/plus-one.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/plus-one/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // TC : O(N)
+    // SC : O(1) + O(N) to return the answer
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        int car = 1;
+        vector<int> ans;
+        for (int i = n - 1; i >= 0; i--) {
+            int last = digits[i];
+            int sum  = last + car;
+            ans.push_back(sum % 10);
+            car = sum / 10;
+        }
+        if (car > 0) ans.push_back(car);
+        reverse(begin(ans), end(ans));
+        return ans;
+    }
+};
