cac7fa4566ba5b883b74da55596f88e6b3dfdb84
Abhishek Kumar <abhixsliet@gmail.com>
2024-06-08 01:39:51 +0530

    007th June 2024 Daily Challenge Solved

--- Leetcode Daily Challenge/012-June-2024/007-replace-words.cpp
+++ Leetcode Daily Challenge/012-June-2024/007-replace-words.cpp
@@ -40,8 +40,77 @@ private:
         
         return result;
     }
+    
+    struct TrieNode {
+        TrieNode* children[26];
+        bool isTerminal;
+    };
+
+    TrieNode* getNode() {
+        TrieNode* newNode = new TrieNode();
+        newNode -> isTerminal = false;
+        for (int i = 0; i < 26; i ++) {
+            newNode -> children[i] = NULL;
+        }
+        return newNode;
+    }
+    
+    TrieNode* root;
+    void insert(string word) {
+        TrieNode* crawler = root;
+
+        for (int i = 0; i < word.length(); i ++) {
+            int idx = word[i] - 'a';
+            if (crawler -> children[idx] == NULL) {
+                crawler -> children[idx] = getNode();
+            }
+            crawler = crawler -> children[idx];
+        }
+
+        crawler -> isTerminal = 1;
+    }
+
+    string search(string word) {
+        TrieNode* crawler = root;
+
+        for (int i = 0; i < word.length(); i ++) {
+            int idx = word[i] - 'a';
+
+            if (crawler -> children[idx] == NULL) {
+                return word;
+            }
+            crawler = crawler -> children[idx];
+            if (crawler -> isTerminal) {
+                return word.substr(0, i + 1);   // shortest root of word
+            }
+        }
+
+        return word;
+    }
+
+    // TC : O(N * L) Where N = Total Words of the dictionary
+    // SC : O(N * L) Where L = Iterating over each chars of the word of dictionary
+    string approach_2(vector<string>& dictionary, string sentence) {
+        root = getNode();
+
+        for (string& word : dictionary) {
+            insert(word);
+        }
+
+        stringstream ss(sentence);
+        string word;
+        string result;
+
+        while (getline(ss, word, ' ')) {
+            result += search(word) + " ";
+        }
+        result.pop_back();
+        return result;
+    }
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        return approach_1(dictionary, sentence);
+        // return approach_1(dictionary, sentence);
+
+        return approach_2(dictionary, sentence);
     }
 };
