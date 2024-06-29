e05d4fbf1ef00ff2af2c26e46b1099c7ac8dfe6d
Abhishek Kumar <abhixsliet@gmail.com>
2024-06-29 18:18:12 +0530

    029th June 2024 POTD Solved

---
+++ GFG POTD/006-June-2024/029-identical-linked-lists.cpp
@@ -0,0 +1,82 @@
+// https://www.geeksforgeeks.org/problems/identical-linked-lists/1
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+bool areIdentical(struct Node *head1, struct Node *head2);
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n1, n2, tmp, d1, d2;
+        struct Node *head1 = NULL, *tail1 = NULL;
+        struct Node *head2 = NULL, *tail2 = NULL;
+        cin >> n1;
+        cin >> d1;
+        head1 = new Node(d1);
+        tail1 = head1;
+        while (n1-- > 1) {
+            cin >> tmp;
+            tail1->next = new Node(tmp);
+            tail1 = tail1->next;
+        }
+        cin >> n2;
+        cin >> d2;
+        head2 = new Node(d2);
+        tail2 = head2;
+        while (n2-- > 1) {
+            cin >> tmp;
+            tail2->next = new Node(tmp);
+            tail2 = tail2->next;
+        }
+
+        areIdentical(head1, head2) ? cout << "true" << endl : cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+/*
+Structure of the node of the linked list is as
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+// Function to check whether two linked lists are identical or not.
+bool areIdentical(struct Node *head1, struct Node *head2) {
+    Node* temp1 = head1;
+    Node* temp2 = head2;
+    
+    while (temp1 != NULL && temp2 != NULL) {
+        if (temp1 -> data != temp2 -> data) {
+            return false;
+        } 
+        temp1 = temp1 -> next;
+        temp2 = temp2 -> next;
+    }
+    
+    return !temp1 && !temp2;
+}
