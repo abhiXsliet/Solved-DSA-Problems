9382237885f48fc4de1e21bd0f365c3b88e522e7
Abhishek Kumar <abhixsliet@gmail.com>
2024-07-07 00:12:51 +0530

    006th July 2024 Daily Challenge Solved In Java

---
+++ Leetcode Daily Challenge/013-July-2024/006-pass-the-pillow.java
@@ -0,0 +1,66 @@
+// https://leetcode.com/problems/pass-the-pillow/
+
+
+
+
+class Solution {
+    public int passThePillow(int n, int time) {
+        // return way_1(n, time);
+
+        // return way_2(n, time);
+
+        return approach_2(n, time);
+    }
+
+    // TC : O(time)
+    // SC : O(1)
+    private static int way_1(int n, int time) {
+        int person    = 1;
+        int cntTime   = 1; 
+        // Change of Direction 
+        int direction = 0;  
+        while (cntTime <= time) {
+            if (direction == 0) {
+                person += 1;
+                if (person == n) {
+                    direction = 1;
+                }
+            } else {
+                person -= 1;
+                if (person == 1) {
+                    direction = 0;
+                }
+            }
+            cntTime += 1;
+        }
+        return person;
+    }
+
+    // TC : O(time)
+    // SC : O(1)
+    private static int way_2(int n, int time) {
+        int person    = 1; 
+        int direction = 1; // direction L ---> R or R ---> L
+        while (time > 0) {
+            if (person + direction >= 1 && person + direction <= n) {
+               person += direction;
+               time --;
+            } else {
+                direction *= -1;
+            }
+        }
+        return person;
+    }
+
+    // TC : O(1)
+    // SC : O(1)
+    private static int approach_2(int n, int time) {
+        int fullRnd  = time / (n - 1);
+        int timeLeft = time % (n - 1);
+
+        if (fullRnd % 2 == 1) {  // Odd Full Round
+            return n - timeLeft;
+        } 
+        return timeLeft + 1; // Even Full Round
+    }
+}
