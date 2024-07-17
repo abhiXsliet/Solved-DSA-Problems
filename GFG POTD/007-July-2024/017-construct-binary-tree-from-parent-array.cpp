f170d20dca6db86ac07a5c31fcf6b24a0c82f22b
Abhishek Kumar <abhixsliet@gmail.com>
2024-07-17 22:39:38 +0530

    017th July 2024 POTD Solved In Cpp

---
+++ GFG POTD/007-July-2024/017-construct-binary-tree-from-parent-array.cpp
@@ -0,0 +1,133 @@
+// https://www.geeksforgeeks.org/problems/construct-binary-tree-from-parent-array/1
+
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+void sort_and_print(vector<int>& v) {
+    sort(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    v.clear();
+}
+
+void printLevelOrder(struct Node* root) {
+    vector<int> v;
+    queue<Node*> q;
+
+    q.push(root);
+
+    Node* next_row = NULL;
+
+    while (!q.empty()) {
+        Node* n = q.front();
+        q.pop();
+
+        if (n == next_row) {
+            sort_and_print(v);
+            next_row = NULL;
+        }
+
+        v.push_back(n->data);
+
+        if (n->left) {
+            q.push(n->left);
+            if (next_row == NULL)
+                next_row = n->left;
+        }
+
+        if (n->right) {
+            q.push(n->right);
+            if (next_row == NULL)
+                next_row = n->right;
+        }
+    }
+    sort_and_print(v);
+}
+
+Node* createTree(int parent[], int n);
+
+/* Driver program to test above function*/
+
+
+// } Driver Code Ends
+/* node structure  used in the program
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    // TC : O(N)
+    // SC : O(N)
+    // Function to construct binary tree from parent array.
+    Node *createTree(vector<int> parent) {
+        unordered_map<int, Node*> mpp;
+        for (int i = 0; i < parent.size(); i ++) {
+            mpp[i] = new Node(i);
+        }
+        
+        Node* root = NULL;
+        for (int i = 0; i < parent.size(); i ++) {
+            if (parent[i] == -1) {
+                root = mpp[i];
+            } else {
+                if (mpp[parent[i]] -> left == NULL)
+                    mpp[parent[i]] -> left  = mpp[i];
+                else 
+                    mpp[parent[i]] -> right = mpp[i];
+            }
+        }
+        return root;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+
+        Solution ob;
+
+        Node* res = ob.createTree(a);
+
+        printLevelOrder(res);
+        cout << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
