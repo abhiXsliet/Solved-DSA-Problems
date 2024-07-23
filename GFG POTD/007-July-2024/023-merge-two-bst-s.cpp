eb8cedd433005e67bcdd8f671344905b30ae1686
Abhishek Kumar <abhixsliet@gmail.com>
2024-07-23 23:19:39 +0530

    023rd July 2024 POTD SOlved In Cpp

---
+++ GFG POTD/007-July-2024/023-merge-two-bst-s.cpp
@@ -0,0 +1,154 @@
+// https://www.geeksforgeeks.org/problems/merge-two-bst-s/1
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree.....
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    void merge(Node *root, vector<int>&v){
+      if(root==NULL)
+        return;
+      merge(root->left,v);
+      v.push_back(root->data);
+      merge(root->right,v);
+    }
+    vector<int> merge(Node *root1, Node *root2) {
+        vector<int>v1,v2,count;
+        merge(root1,v1);
+        merge(root2,v2);
+        int i=0,j=0;
+        while(i<v1.size() && j<v2.size())
+        {
+            if(v1[i]<=v2[j])
+            count.push_back(v1[i++]);
+            else
+            count.push_back(v2[j++]);
+        }
+        while(i<v1.size())
+        count.push_back(v1[i++]);
+        while(j<v2.size())
+        count.push_back(v2[j++]);
+      return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root1 = buildTree(s);
+
+        getline(cin, s);
+        Node* root2 = buildTree(s);
+
+        // getline(cin, s);
+        Solution obj;
+        vector<int> vec = obj.merge(root1, root2);
+        for (int i = 0; i < vec.size(); i++)
+            cout << vec[i] << " ";
+        cout << endl;
+        /// cout<<"~"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
