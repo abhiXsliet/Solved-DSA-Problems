20254cc2bc8bd02ce0e792703dd973ace346e1c4
Abhishek Kumar <abhixsliet@gmail.com>
2024-07-31 22:41:19 +0530

    023rd July 2024 Daily Challenge Solved In Cpp

---
+++ Leetcode Daily Challenge/013-July-2024/023-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/sort-array-by-increasing-frequency/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        vector<int> cnt(201);
+        for (int v : nums) {
+            ++cnt[v + 100];
+        }
+        sort(nums.begin(), nums.end(), [&](const int a, const int b) {
+            if (cnt[a + 100] == cnt[b + 100]) return a > b;
+            return cnt[a + 100] < cnt[b + 100];
+        });
+        return nums;
+    }
+};
