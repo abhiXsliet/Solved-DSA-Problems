b430ac521ebd57b4814457ee58c0552109edaf06
Abhishek Kumar <abhixsliet@gmail.com>
2024-10-03 20:17:36 +0530

    003rd October 2024 POTD Solved

---
+++ GFG POTD/011-October-2024/003-majority-vote.cpp
@@ -0,0 +1,78 @@
+// https://www.geeksforgeeks.org/problems/majority-vote/1
+
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the majority elements in the array
+    vector<int> findMajority(vector<int>& nums) {
+        int n = nums.size();
+        
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 =-1, ele2 =-1;
+        
+        for (int i = 0; i < n; i ++) {
+            if      (ele1 == nums[i]) cnt1 ++;
+            else if (ele2 == nums[i]) cnt2 ++;
+            else if (cnt1 == 0) {
+                ele1 = nums[i];
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                ele2 = nums[i];
+                cnt2 = 1;
+            } 
+            else {  // catch
+                cnt1 --;
+                cnt2 --;
+            }
+        }
+        
+        vector<int> result;
+
+        cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < n; i ++) {
+            if (ele1 == nums[i]) cnt1 ++;
+            if (ele2 == nums[i]) cnt2 ++;
+        }
+        
+        if (cnt1 >= n/3 + 1) result.push_back(ele1);
+        if (cnt2 >= n/3 + 1) result.push_back(ele2);
+        
+        if (result.size() == 0) {
+            result.push_back(-1);
+        } 
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<int> ans = ob.findMajority(nums);
+        for (auto &i : ans)
+            cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
