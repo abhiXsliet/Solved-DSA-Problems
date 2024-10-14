c18a31263aa9a94cc5310be9903f182e38c43f83
Abhishek Kumar <abhixsliet@gmail.com>
2024-10-14 10:00:56 +0530

    014th October 2024 POTD Solved

---
+++ GFG POTD/011-October-2024/014-count-nodes-of-linked-list.java
@@ -0,0 +1,66 @@
+// https://www.geeksforgeeks.org/problems/count-nodes-of-linked-list/1
+
+
+
+
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+
+    Node(int a) {
+        data = a;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+/*Complete the function below*/
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}*/
+
+class Solution {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        int cnt = 0;
+        Node temp = head;
+        while (temp != null) {
+            cnt += 1;
+            temp = temp.next;
+        }
+        return cnt;
+    }
+}
+
+//{ Driver Code Starts.
+class LinkedList {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.getCount(head));
+        }
+    }
+}
+// } Driver Code Ends
