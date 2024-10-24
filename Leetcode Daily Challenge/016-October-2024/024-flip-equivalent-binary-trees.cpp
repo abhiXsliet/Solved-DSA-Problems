4441c6ed72f523f0b91df2c45076ece0fd577494
Abhishek Kumar <abhixsliet@gmail.com>
2024-10-24 23:05:57 +0530

    024th October 2024 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/016-October-2024/024-flip-equivalent-binary-trees.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/flip-equivalent-binary-trees/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    // TC : O(N)
+    // SC : O(N) Recursive Depth
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if (!root1 && !root2) return true;
+        if (!root1 || !root2) return false;
+
+        if (root1 -> val == root2 -> val) {
+            bool withoutFlip = flipEquiv(root1 -> left, root2 -> left)  && flipEquiv(root1 -> right, root2 -> right);
+            bool withFlip    = flipEquiv(root1 -> left, root2 -> right) && flipEquiv(root1 -> right, root2 -> left);
+
+            return withoutFlip || withFlip;
+        }
+        // else not equal
+        return false;   
+    }
+};
