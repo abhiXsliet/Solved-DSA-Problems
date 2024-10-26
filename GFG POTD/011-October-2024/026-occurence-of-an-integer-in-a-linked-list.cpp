17f5cc404ede188452af051b438b4048d11cdc19
Abhishek Kumar <abhixsliet@gmail.com>
2024-10-26 11:08:09 +0530

    026th October 2024 POTD Solved

---
+++ GFG POTD/011-October-2024/026-occurence-of-an-integer-in-a-linked-list.cpp
@@ -0,0 +1,78 @@
+// https://www.geeksforgeeks.org/problems/occurence-of-an-integer-in-a-linked-list/1
+
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+  Node is defined as
+struct node
+{
+    int data;
+    struct node* next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}*head;
+*/
+class Solution {
+  public:
+    int count(struct Node* head, int key) {
+        Node* temp = head;
+        int countKey = 0;
+        while (temp) {
+            if (temp -> data == key) countKey ++;
+            temp = temp -> next;
+        }
+        return countKey;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        int key;
+        cin >> key;
+        cin.ignore();
+        Solution ob;
+        cout << ob.count(head, key) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
