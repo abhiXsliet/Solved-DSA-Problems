b99866cab9bea055f74dff5e1c6d96113f2cf8c4
Abhishek Kumar <abhixsliet@gmail.com>
2024-10-31 23:48:27 +0530

    031st October 2024 POTD Solved

---
+++ GFG POTD/011-October-2024/031-insert-in-sorted-way-in-a-sorted-dll.cpp
@@ -0,0 +1,104 @@
+// https://www.geeksforgeeks.org/problems/insert-in-sorted-way-in-a-sorted-dll/1
+
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *prev, *next;
+};
+
+struct Node* getNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = newNode->next = NULL;
+    return newNode;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+// } Driver Code Ends
+
+/*structure of the node of the DLL is as
+struct Node {
+    int data;
+    struct Node* prev, *next;
+};
+*/
+
+// function should insert a new node in sorted way in
+// a sorted doubly linked list
+// Return the head after insertion
+class Solution {
+  public:
+    Node* sortedInsert(Node* head, int x) {
+        Node* newNode = new Node();
+        newNode -> data = x;
+        
+        if (!head || head -> data > x) {
+            newNode -> next = head;
+            return newNode;
+        }
+        
+        Node* curr = head;
+        Node* prev = NULL;
+        
+        while (curr && curr -> data <= x) {
+            prev = curr;
+            curr = curr -> next;
+        }
+        
+        newNode -> next = curr;
+        if (prev) prev -> next = newNode;
+        
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+
+    while (t--) {
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+
+        stringstream ss(input);
+        Node *head = nullptr, *tail = nullptr;
+        int x;
+
+        if (ss >> x) {
+            head = getNode(x);
+            tail = head;
+        }
+
+        while (ss >> x) {
+            tail->next = getNode(x);
+            tail->next->prev = tail;
+            tail = tail->next;
+        }
+
+        int valueToInsert;
+        cin >> valueToInsert;
+        cin.ignore(); // Ignore the newline character after the value
+
+        Solution obj;
+        head = obj.sortedInsert(head, valueToInsert);
+        printList(head);
+    }
+
+    return 0;
+}
+// } Driver Code Ends
