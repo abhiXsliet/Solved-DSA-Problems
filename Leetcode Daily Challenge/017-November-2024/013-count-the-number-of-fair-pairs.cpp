5ff100abb511d8c5a7c9a194964af0972cf414b3
Abhishek Kumar <abhixsliet@gmail.com>
2024-11-13 17:28:15 +0530

    013th November 2024 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/017-November-2024/013-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/count-the-number-of-fair-pairs/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // TC : O(N*log(N))
+    // SC : O(1)
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+
+        long long cntPairs = 0;
+        for (int i = 0; i < n; i ++) {
+            // Gives the first index which is NOT LESS than (lower - nums[i])
+            int lower_idx = lower_bound(begin(nums) + i + 1, end(nums), lower - nums[i]) - begin(nums);
+            int lower_cnt = lower_idx - 1 - i;
+
+            // Gives the first index which is GREATER than (upper - nums[i])
+            int upper_idx = upper_bound(begin(nums) + i + 1, end(nums), upper - nums[i]) - begin(nums);
+            int upper_cnt = upper_idx - 1 - i;
+
+            cntPairs += (upper_cnt - lower_cnt);
+        }
+
+        return cntPairs;
+    }
+};
