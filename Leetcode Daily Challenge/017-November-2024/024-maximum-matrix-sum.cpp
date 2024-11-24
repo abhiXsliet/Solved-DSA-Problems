722702ed8f1e04c2a855366fdec9cfb64ed929cc
Abhishek Kumar <abhixsliet@gmail.com>
2024-11-24 23:09:53 +0530

    024th November 2024 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/017-November-2024/024-maximum-matrix-sum.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/maximum-matrix-sum/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // TC : O(M * N)
+    // SC : O(1)
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        long long result = 0;
+        int cntNegatives = 0;
+        int minNumber = INT_MAX;
+        for (int i = 0; i < m; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (matrix[i][j] < 0) {
+                    cntNegatives ++;
+                }
+                result   += abs(matrix[i][j]);
+                minNumber = min(minNumber, abs(matrix[i][j]));
+            }
+        }
+        if (cntNegatives % 2) {
+            result -= 2*minNumber;
+        }
+        return result;
+    }
+};
