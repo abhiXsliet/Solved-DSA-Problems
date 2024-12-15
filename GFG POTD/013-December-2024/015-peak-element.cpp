4dfce0dfdf3daf5e48865d4e9d684b7687bbd4d7
Abhishek Kumar <abhixsliet@gmail.com>
2024-12-15 20:12:21 +0530

    015th December 2024 POTD Solved

---
+++ GFG POTD/013-December-2024/015-peak-element.cpp
@@ -0,0 +1,72 @@
+// https://www.geeksforgeeks.org/problems/peak-element/1
+
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // TC : O(log(N))
+    // SC : O(1)
+    int peakElement(vector<int> &arr) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high= mid;
+            }
+        }
+        return high;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution ob;
+        int idx = ob.peakElement(a);
+        int n = a.size();
+        bool f = 0;
+        if (idx < 0 and idx >= n)
+            cout << "false" << endl;
+        else {
+            if (n == 1 and idx == 0)
+                f = 1;
+            else if (idx == 0 and a[0] > a[1])
+                f = 1;
+            else if (idx == n - 1 and a[n - 1] > a[n - 2])
+                f = 1;
+            else if (a[idx] > a[idx + 1] and a[idx] > a[idx - 1])
+                f = 1;
+            else
+                f = 0;
+            if (f)
+                cout << "true" << endl;
+            else
+                cout << "false" << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
