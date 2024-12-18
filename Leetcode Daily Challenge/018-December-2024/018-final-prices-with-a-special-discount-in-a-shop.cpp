8831b35cd148e459648b08f7922f0410adcffff9
Abhishek Kumar <abhixsliet@gmail.com>
2024-12-18 23:24:27 +0530

    018th Decmeber 2024 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/018-December-2024/018-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    // TC : O(N)
+    // SC : O(N) 
+    vector<int> solve_optimal(vector<int>& prices, int n) {
+        vector<int> result(n);
+        stack<int> stk;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while(!stk.empty() && !(stk.top() <= prices[i])) {
+                stk.pop();
+            }
+
+            if (stk.empty()) result[i] = prices[i];
+            else result[i] = prices[i] - stk.top();
+
+            stk.push(prices[i]);
+        }
+        return result;
+    }
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        return solve_optimal(prices, prices.size());
+    }
+};
