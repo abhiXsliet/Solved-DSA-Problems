b727f552192f8aa4cf61893e2a14420ef3e8e13b
Abhishek Kumar <abhixsliet@gmail.com>
2024-12-20 01:48:30 +0530

    019th December 2024 POTD Modified

--- GFG POTD/013-December-2024/019-kth-missing-positive-number-in-a-sorted-array.cpp
+++ GFG POTD/013-December-2024/019-kth-missing-positive-number-in-a-sorted-array.cpp
@@ -29,14 +29,44 @@ class Solution {
         return -1;
     }
     
+    // TC : O(N)
+    // SC : O(1)
+    int better(vector<int>& arr, int k) {
+        int n = arr.size();
+        int num = 1;
+        int cnt = 0;
+        for (int i = 0; i < n; i ++) {
+            if (num != arr[i]) {
+                cnt ++;
+                i --;
+                if (cnt == k) return num;
+            } 
+            num ++;
+        }
+        return num + (k - cnt) - 1;
+    }
+    
     // TC : O(log(N))
     // SC : O(1)
     int optimal(vector<int>& arr, int k) {
-        
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int missing_nos = arr[mid] - (mid + 1);
+            if (missing_nos < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low + k;
     }
   public:
     int kthMissing(vector<int> &arr, int k) {
         // return brute(arr, k);
+        
+        // return better(arr, k);
         
         return optimal(arr, k);
     }
