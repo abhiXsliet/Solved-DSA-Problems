dda9bc016537a9677e5e54fdfd156199500d3d20
Abhishek Kumar <abhixsliet@gmail.com>
2025-02-22 12:46:20 +0530

    004th October 2023 Daily Challenge Modified

--- Leetcode Daily Challenge/004-October/004-design-hashmap.cpp
+++ Leetcode Daily Challenge/004-October/004-design-hashmap.cpp
@@ -30,6 +30,77 @@ public:
     }
 };
 
+
+// --------------------------------- OPTIMAL ------------------------------------------------------------------
+class MyHashMap
+{
+public:
+  vector<list<pair<int, int>>> buckets;
+  int size;
+  MyHashMap()
+  {
+    size = 10000;
+    buckets.resize(size);
+  }
+
+  int getIndex(int key)
+  { // a simple hash function
+    return key % size;
+  }
+
+  void put(int key, int value)
+  {
+    int bucket_no = getIndex(key);
+    list<pair<int, int>> &chain = buckets[bucket_no];
+
+    for (auto &it : chain)
+    {
+      if (it.first == key)
+      { // if found then update value of key
+        it.second = value;
+        return;
+      }
+    }
+    chain.emplace_back(key, value);
+  }
+
+  int get(int key)
+  {
+    int bucket_no = getIndex(key);
+    list<pair<int, int>> &chain = buckets[bucket_no];
+
+    if (chain.empty())
+      return -1;
+
+    for (auto &it : chain)
+    {
+      if (it.first == key)
+      {
+        return it.second;
+      }
+    }
+    return -1;
+  }
+
+  void remove(int key)
+  {
+    int bucket_no = getIndex(key);
+    list<pair<int, int>> &chain = buckets[bucket_no];
+
+    if (chain.empty())
+      return;
+
+    for (list<pair<int, int>>::iterator it = chain.begin(); it != chain.end(); it++)
+    {
+      if (it->first == key)
+      {
+        chain.erase(it);
+        return;
+      }
+    }
+  }
+};
+
 /**
  * Your MyHashMap object will be instantiated and called as such:
  * MyHashMap* obj = new MyHashMap();
