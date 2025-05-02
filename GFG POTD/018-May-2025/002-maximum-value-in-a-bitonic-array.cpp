032eb840db42824fe6b90a9c68d77b5f53a11a57
Abhishek Kumar <abhixsliet@gmail.com>
2025-05-02 16:35:13 +0530

    002nd May 2025 POTD Solved

---
+++ GFG POTD/018-May-2025/002-maximum-value-in-a-bitonic-array.cpp
@@ -0,0 +1,54 @@
+// https://www.geeksforgeeks.org/problems/maximum-value-in-a-bitonic-array3001/1
+
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+    int findMaximum(vector<int> &arr) {
+        int n = arr.size();
+        for (int i = 0; i < n - 1; i ++) {
+            if (arr[i] > arr[i + 1]) {
+                return arr[i];
+            }
+        }
+        return arr[n - 1];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        int value;
+
+        // Read the entire line of integers
+        string line;
+        // cin.ignore(); // Ignore the newline after the test case input
+        getline(cin, line);
+        stringstream ss(line);
+
+        // Parse integers and add them to the vector
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        Solution ob;
+        auto ans = ob.findMaximum(arr);
+        cout << ans << "\n";
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
