9e73dc28e02575a3783e2652281beb485a323f24
Abhishek Kumar <abhixsliet@gmail.com>
2025-05-31 12:21:15 +0530

    031st May 2025 POTD Solved

---
+++ GFG POTD/018-May-2025/031-kth-element-in-matrix.cpp
@@ -0,0 +1,37 @@
+// https://www.geeksforgeeks.org/problems/kth-element-in-matrix/1
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  private:
+    // returns the cnt of no. less than or equal to tar
+    int cntLessThanK(vector<vector<int>> &matrix, int tar, int k) {
+        int n = matrix.size();
+        int cnt = 0;    
+        for (int i = 0; i < n; i ++) {
+            cnt += upper_bound(begin(matrix[i]), end(matrix[i]), tar) - begin(matrix[i]);
+        }
+        return cnt;
+    }
+  public:
+    // TC : O(log(R) * N*log(N))
+    // SC : O(1)
+    int kthSmallest(vector<vector<int>> &matrix, int k) {
+        int n = matrix.size();
+        int low  = matrix[0][0];
+        int high = matrix[n - 1][n - 1];
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (cntLessThanK(matrix, mid, k) >= k) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
