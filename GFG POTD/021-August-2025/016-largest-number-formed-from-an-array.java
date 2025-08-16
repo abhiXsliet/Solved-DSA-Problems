5aa60644030f29761581d985e9bd9e5793ea051b
Abhishek Kumar <abhixsliet@gmail.com>
2025-08-16 16:00:26 +0530

    016th August 2025 POTD Solved

---
+++ GFG POTD/021-August-2025/016-largest-number-formed-from-an-array.java
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/largest-number-formed-from-an-array1117/1
+
+
+
+
+import java.util.*;
+
+class Solution {
+    // TC : O(N * log(N))
+    // SC : O(N)
+    public String findLargest(int[] arr) {
+        int n = arr.length;
+        
+        String[] nums = new String[n];
+        for (int i = 0; i < n; i ++) {
+            nums[i] = String.valueOf(arr[i]);
+        }
+        
+        Arrays.sort(nums, (a, b) -> (b + a).compareTo(a + b));
+        
+        if (nums[0].equals("0")) return "0";
+        
+        StringBuilder largestNum = new StringBuilder();
+        for (String num : nums) {
+            largestNum.append(num);
+        }
+        return largestNum.toString();
+    }
+}
