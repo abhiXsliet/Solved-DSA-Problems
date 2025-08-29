418086881591ead362599d4c25f03dfda8218434
Abhishek Kumar <abhixsliet@gmail.com>
2025-08-29 13:15:30 +0530

    029th August 2025 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/026-August-2025/029-alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    // TC : O(1)
+    // SC : O(1)
+    long long approach_1(int n, int m) {
+        long nEven = n / 2;
+        long nOdd  = n - nEven;
+
+        long mEven = m / 2;
+        long mOdd  = m - mEven;
+
+        return (nEven * mOdd) + (nOdd * mEven);
+    }
+
+    // TC : O(1)
+    // SC : O(1)
+    long long approach_2(int n, int m) {
+        return ((long)n * m) / 2;
+    }
+public:
+    long long flowerGame(int n, int m) {
+        // return approach_1(n, m);
+        return approach_2(n, m);
+    }
+};
