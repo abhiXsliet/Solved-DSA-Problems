2674391e084cce8d5c02a200ec1963bd2afbab08
Abhishek Kumar <abhixsliet@gmail.com>
2025-09-20 23:08:20 +0530

    019th September 2025 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/027-September-2025/019-design-spreadsheet.cpp
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/design-spreadsheet/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// TC : O(rows) for constructor, O(L) for getValue() where L = formula.length()
+class Spreadsheet {
+public:
+    vector<vector<int>> sheet;
+    Spreadsheet(int rows) {
+        sheet.assign(rows, vector<int>(26, 0));
+    }
+    
+    void setCell(string cell, int value) {
+        int c = cell[0] - 'A';
+        int r = stoi(cell.substr(1)) - 1;
+        sheet[r][c] = value;
+    }
+    
+    void resetCell(string cell) {
+        int c = cell[0] - 'A';
+        int r = stoi(cell.substr(1)) - 1;
+        sheet[r][c] = 0;
+    }
+
+    int solve(string &str) {
+        if (isdigit(str[0])) {
+            return stoi(str);
+        }
+
+        int c = str[0] - 'A';
+        int r = stoi(str.substr(1)) - 1;
+        return sheet[r][c];
+    }
+    
+    int getValue(string formula) {
+        int plusIdx = formula.find_first_of('+');
+    
+        string left  = formula.substr(1, plusIdx - 1);
+        string right = formula.substr(plusIdx + 1); 
+
+        return solve(left) + solve(right);   
+    }
+};
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
