d417c6b4508c7b54ecc5d97f5c5faee3919c73c9
Abhishek Kumar <abhixsliet@gmail.com>
2025-10-03 00:52:29 +0530

    002nd October 2025 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/028-October-2025/002-water-bottles-ii.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/water-bottles-ii/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    typedef long long ll;
+    
+    // TC : O(numBottles)
+    // SC : O(1)
+    int approach_1(int numBottles, int numExchange) {
+        int drink       = numBottles;
+        ll emptyBottles = numBottles;
+
+        while(emptyBottles) {
+            emptyBottles = emptyBottles - numExchange;
+            if (emptyBottles >= 0) {
+                numExchange  += 1;
+                drink        += 1;
+                emptyBottles += 1;
+            }
+            else break;
+        }
+
+        return drink;
+    }
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        return approach_1(numBottles, numExchange);
+    }
+};
