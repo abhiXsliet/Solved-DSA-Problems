98eaeabce66a6a1a2e1ca7306661f7098f8e2434
Abhishek Kumar <abhixsliet@gmail.com>
2025-10-10 23:56:12 +0530

    010th October 2025 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/028-October-2025/009-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/taking-maximum-energy-from-the-mystic-dungeon/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // TC : O(N)
+    // SC : O(1)
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n = energy.size();
+        vector<int> store = energy;
+        
+        for (int i = k; i < n; i++) {
+            store[i] = max(store[i], store[i] + store[i - k]);
+        }
+
+        int ans = INT_MIN;
+        for (int i = n - k; i < n; i++) {
+            ans = max(ans, store[i]);
+        }
+        return ans;
+    }
+};
