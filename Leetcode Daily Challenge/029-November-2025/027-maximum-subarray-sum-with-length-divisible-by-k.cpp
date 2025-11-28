c5d68934bfb4b8c4d66a3a44a68469767f4e8a40
Abhishek Kumar <abhixsliet@gmail.com>
2025-11-28 23:37:31 +0530

    027th November 2025 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/029-November-2025/027-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/maximum-subarray-sum-with-length-divisible-by-k/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<long long> prefSum(n);
+        prefSum[0] = nums[0];
+
+        for(int i = 1; i < n; i++) {
+            prefSum[i] = prefSum[i-1] + nums[i];
+        }
+
+        long long result = LLONG_MIN;
+
+        for(int start = 0; start < k; start++) {
+            long long currSum = 0;
+
+            int i = start;
+
+            while(i < n && i + k - 1 < n) {
+                int j = i + k - 1;
+
+                long long subSum = prefSum[j] - ((i > 0) ? prefSum[i-1] : 0);
+
+                currSum = max(subSum, currSum + subSum);
+
+                result = max(result, currSum);
+
+                i += k;
+            }
+        }
+
+        return result;
+    }
+};
