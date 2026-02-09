7291a6ccce9859e007408cf06d8f3cd413e5859c
Abhishek Kumar <abhixsliet@gmail.com>
2026-02-09 23:44:28 +0530

    009th February 2026 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/032-February-2026/009-balance-a-binary-search-tree.cpp
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/balance-a-binary-search-tree/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    void morrisTraversal(TreeNode* root, vector<int>& store) {
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr -> left == NULL) {
+                store.push_back(curr -> val);
+                curr = curr -> right;
+            } else {
+                TreeNode* leftChild = curr -> left;
+                while (leftChild -> right != NULL) {
+                    leftChild = leftChild -> right;
+                }
+                leftChild -> right = curr;
+
+                // Mark NULL : curr -> left to avoid the infinite looping
+                TreeNode* temp = curr;
+                curr           = curr -> left;
+                temp -> left   = NULL;
+            }
+        }
+        return;
+    }
+
+    TreeNode* buildBalancedBST(vector<int>& store, int left, int right) {
+        if (left > right) return NULL;
+
+        int mid = (left + right) >> 1;
+        TreeNode* root = new TreeNode(store[mid]);
+
+        root -> left  = buildBalancedBST(store, left, mid - 1);
+        root -> right = buildBalancedBST(store, mid + 1, right);
+
+        return root;
+    }
+
+    TreeNode* approach_1(TreeNode* root) {
+        vector<int> store;
+        morrisTraversal(root, store);
+        return buildBalancedBST(store, 0, store.size() - 1);
+    }
+public:
+    TreeNode* balanceBST(TreeNode* root) {
+        return approach_1(root);
+    }
+};
