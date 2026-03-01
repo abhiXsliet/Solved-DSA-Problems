c96a43e43137c4c184121e2bbda111e549c56520
Abhishek Kumar <abhixsliet@gmail.com>
2026-03-01 23:57:43 +0530

    001st March 2026 POTD Solved

---
+++ GFG POTD/033-March-2026/001-move-all-zeroes-to-end-of-array.cpp
@@ -0,0 +1,61 @@
+// https://www.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// User function template for C++
+class Solution {
+  private:
+    // TC : O(N)
+    // SC : O(1)
+    void solve_1(vector<int>& arr) {
+        int n = arr.size();
+        int i = 0, j = 1;
+        while (j < n) {
+            if (arr[i] != 0 && arr[j] != 0) {
+                i ++, j ++;
+            } else if (arr[i] == 0 && arr[j] != 0) {
+                swap(arr[i], arr[j]);
+                i ++, j ++;   
+            }
+            while (j < n && arr[j] == 0) j ++;
+            while (i < j && arr[i] != 0) i ++;
+        }
+    }
+    
+    // TC : O(N)
+    // SC : O(1)
+    void solve_2(vector<int>& arr) {
+        int n = arr.size();
+        int i = 0, j = 1;
+        while (j < n) {
+            if (arr[i] == 0 && arr[j] != 0) {
+                swap(arr[i], arr[j]);
+                i ++, j ++;   
+            } else if (arr[i] == 0 && arr[j] == 0) j ++;
+            else i ++, j ++;
+        }
+    }
+    
+    // TC : O(N)
+    // SC : O(1)
+    void solve_3(vector<int>& arr) {
+        int i = 0;
+        for (int j = 0; j < arr.size(); j ++) {
+            if (arr[j]) {
+                swap(arr[i], arr[j]);
+                i ++;
+            }
+        }
+    }
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // solve_1(arr);
+        // solve_2(arr);
+        solve_3(arr);
+    }
+};
