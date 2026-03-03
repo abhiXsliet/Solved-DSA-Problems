d7ac0542a772feb2b73f83b67de2a6e4aa3386b0
Abhishek Kumar <abhixsliet@gmail.com>
2026-03-03 23:41:10 +0530

    003rd March 2026 POTD Solved

---
+++ GFG POTD/033-March-2026/003-fruit-into-baskets.cpp
@@ -0,0 +1,30 @@
+// https://www.geeksforgeeks.org/problems/fruit-into-baskets-1663137462/1/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int totalElements(vector<int> &arr) {
+        int n = arr.size();
+        
+        unordered_map<int, int> mpp;
+        int i = 0, j = 0, len = 0;
+        while (j < n) {
+            mpp[arr[j]] ++;
+            while (i < j && mpp.size() > 2) {
+                mpp[arr[i]] --;
+                if (!mpp[arr[i]]) mpp.erase(arr[i]);
+                i ++;
+            }
+            
+            len = max(len, j - i + 1);
+            
+            j ++;
+        }
+        return len;
+    }
+};
