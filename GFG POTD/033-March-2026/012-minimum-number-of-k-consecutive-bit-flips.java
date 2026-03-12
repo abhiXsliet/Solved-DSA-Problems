28af52b73cdf1dafd74f47de810c78e002f7199c
Abhishek Kumar <abhixsliet@gmail.com>
2026-03-12 23:41:45 +0530

    012th March 2026 POTD Solved

---
+++ GFG POTD/033-March-2026/012-minimum-number-of-k-consecutive-bit-flips.java
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/minimum-number-of-k-consecutive-bit-flips--171650/1/
+
+
+
+
+class Solution {
+    public int kBitFlips(int[] arr, int k) {
+        int n = arr.length;
+        int[] pSum = new int[n];
+        int operations = 0;
+        
+        for (int i = 0; i < n; i ++) {
+            if (i > 0) pSum[i] += pSum[i - 1];
+            
+            boolean isToggled = (pSum[i] % 2 != 0);
+            
+            if ((i > n - k) && ((arr[i] == 1 && isToggled) || (arr[i] == 0 && !isToggled)))
+                return -1;
+            
+            if ((arr[i] == 1 && isToggled) || (arr[i] == 0 && !isToggled)) {
+                pSum[i] += 1;
+                if (i + k < n) pSum[i + k] -= 1;
+                operations += 1;
+            }
+        }
+        
+        return operations;
+    }
+}
