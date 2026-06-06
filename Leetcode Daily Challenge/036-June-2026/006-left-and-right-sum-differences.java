6cde443522f51147ff6f93e229ada1a0ca1c26b4
Abhishek Kumar <abhixsliet@gmail.com>
2026-06-06 23:19:12 +0530

    006th June 2026 Daily Challenge Solved

---
+++ Leetcode Daily Challenge/036-June-2026/006-left-and-right-sum-differences.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/left-and-right-sum-differences/
+
+
+
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int l = 0, r = 0;
+        for (int x : nums) {
+            r += x;
+        }
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i) {
+            r -= nums[i];
+            ans[i] = Math.abs(l - r);
+            l += nums[i];
+        }
+        return ans;
+    }
+}
